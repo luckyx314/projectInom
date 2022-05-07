e094157262ed234fcf4d1f8266d3981e735cace1
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2022-05-07 21:17:49 +0800

    styled: donationpage component

--- client/src/components/DonationPage/DonationPage.js
+++ client/src/components/DonationPage/DonationPage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./DonationPage.css";
 
 const DonationPage = () => {
-  return (
-    <div>DonationPage</div>
-  )
-}
+    return (
+        <div className="donation-page-parent">
+            <div className="donation-wrapper">
+                <div className="donation-contents">
+                    <div className="shape-container">
+                        <div className="rectangle-shape">
+                            <p className="give-txt">GIVE</p>
+                            <div className="peso-container">
+                                <p className="peso-sign">₱</p>
+                                <p className="piso">1</p>
+                            </div>
+                        </div>
+                        <h3>lorem ipsum</h3>
+                    </div>
+                    <div>
+                        <h2>Lorem ipsum dolorendi.</h2>
+                        <h2>Lorem ipsum dolor sit amet .</h2>
+                        <h2>Lorem iasdi.</h2>
+                    </div>
+                    <button>DONATE</button>
+                </div>
+                <div className="volunteer-statement-container ">
+                    <p>
+                        Another way you can help us <br /> reach this goal is to{" "}
+                        <button>VOLUNTEER</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default DonationPage
+export default DonationPage;
